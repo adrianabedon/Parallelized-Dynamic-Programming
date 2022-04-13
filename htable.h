59784807fc22f39a1638829c5b02b436cbe49857
Nikita Basu <nbasu@andrew.cmu.edu>
2022-04-13 13:56:34 -0400

    Part 2: Putting files back

---
+++ htable.h
@@ -0,0 +1,61 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <atomic>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define EMPTY 0
+
+using namespace std;
+
+typedef struct hash_table *htable_t;
+typedef struct table_entry entry_t;
+
+// keys and values are primitive types
+typedef int key;
+typedef int value;
+
+typedef uint32_t (*hash_fn) (key);
+
+struct table_entry {
+  key k;
+  value v;
+};
+
+struct hash_table {
+  uint32_t capacity;
+  uint32_t size;
+	atomic<entry_t> *table;
+  hash_fn hf;
+};
+
+/* ht_new returns a hash table with an initial capacity = nextPow2(capacity) */ 
+htable_t ht_new(uint32_t capacity, hash_fn hf);
+//@requires hf != NULL;
+//@ensures \result != NULL;
+
+/* Returns the value associated with the key k in the hash table */
+int ht_lookup(htable_t H, key k);
+//@requires H != NULL;
+//@ensures \result == EMPTY || \result > 0;
+
+/* Inserts the key-value pair (k,v) into the hash table */
+void ht_insert(htable_t H, key k, value v);
+//@requires H != NULL;
+
+/* Frees the hash table and all underlying structures */
+void ht_free(htable_t H);
+//@requires H != NULL;
+
+#endif
+
+/* RESOURCES REFERENCED:
+    https://preshing.com/20130605/the-worlds-simplest-lock-free-hash-table/
+    https://web.stanford.edu/class/ee380/Abstracts/070221_LockFreeHash.pdf
+    https://github.com/aappleby/smhasher
+    http://www.polarsparc.com/pdf/PThreads.pdf
+    https://www.cs.cmu.edu/afs/cs/academic/class/15492-f07/www/pthreads.html#SYNCHRONIZATION
+*/
